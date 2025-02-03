a5d4f331e3f42a90ba041029eb52ba9912ae3c5e
pablitodon <pavelkazakevich040196@gmail.com>
2025-02-03 15:21:28 +0300

    feat:add WrapperComponent to Form Login/Register

---
+++ src/shared/LoginRegisterWrapper.vue
@@ -0,0 +1,39 @@
+<template>
+  <main class="wrapper">
+    <slot></slot>
+  </main>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+@import '~/assets/styles/mixins';
+
+.wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 100%;
+  min-height: 100vh;
+  background: var(--grey-bg);
+  border: 0.0625rem solid var(--black);
+  border-radius: 2rem;
+  padding: 2rem;
+
+  @include respond-to-desktop {
+    padding: 1.5rem;
+  }
+
+  @include respond-to-laptop {
+    padding: 1rem;
+  }
+
+  @include respond-to-tablet {
+    padding: 0.75rem;
+  }
+
+  @include respond-to-phone {
+    height: 100vh;
+  }
+}
+</style>
