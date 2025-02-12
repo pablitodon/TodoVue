d77aa9961f7ca06e548faa5d04eca6df04a265ed
pablitodon <pavelkazakevich040196@gmail.com>
2025-02-12 21:47:34 +0300

    feat: setup of monorepository

--- packages/todo-crm/src/app/main.ts
+++ packages/todo-crm/src/app/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import '~/assets/styles/globals.scss';
+import '~/assets/styles/variables.scss';
 import Vue3Toastify from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
 
